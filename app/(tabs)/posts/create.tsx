f5e1f5fa289ad02c29837e37f55ae9848fa24ba0
caueeugenio <caue_eugenio@hotmail.com>
2024-11-12 18:31:45 -0300

    feat: adjusting background color

--- app/(tabs)/posts/create.tsx
+++ app/(tabs)/posts/create.tsx
@@ -107,7 +107,7 @@ export default function CreatePost() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: Colors.white,
     alignItems: 'center',
     justifyContent: 'center',
   },
